4f67c564d3fdd9641183157c5452a51f82da55f2
KyleCrow <m.cloet@ecst.org>
2019-03-20 11:26:17 +0100

    sensor_spray.h mis a jour

--- sensor_spray.h
+++ sensor_spray.h
@@ -1,3 +1,24 @@
 //Partie de Raphael: Conditions de pulvérisation
+const int relayPin= 8;               //Le relais sera placé sur la broche D5
+float Batterie;                      //Variable du niveau de batterie (%)
+bool Desherbant;                     //Variable du niveau de désherbant
+bool Herbe;                          //Variable de la détection de mavaises herbes
 
+void setup() 
+{
+ pinMode(relayPin, OUTPUT);          //Le relais est configuré pour les signaux de sortie
+}
 
+void loop() 
+{
+ if ((B!=0) && (D!=0) && (H!=0))     //Si une mauvaise herbe a été détecté et que les niveaux de batterie et de désherbant sont suffisants 
+ {
+  digitalWrite(relayPin, HIGH);      //Le relais actionne la pompe
+  delay(3000);
+  digitalWrite(relayPin, LOW);
+ }
+ else                                //Si aucune mauvaise herbe n'a été détecté ou que l'un des niveaux de batterie ou de désherbant sont insuffisants  
+ {
+  digitalWrite(relayPin, LOW);       //Le relais n'actionne pas la pompe        
+ }
+}
